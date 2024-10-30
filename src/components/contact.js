d141a8c7c8bb1132484950605933af7a98b1104d
Lars Knoke <larz.knoke@gmail.com>
2024-10-30 22:18:25 +0100

    opti & facebook

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Contact() {
   return (
@@ -11,13 +12,15 @@ function Contact() {
         className="w-28 md:w-40"
       />
       <div className="flex flex-row gap-5 items-center">
-        <Image
-          src="facebook.svg"
-          alt="Facebook Logo"
-          width="218"
-          height="33"
-          className=" w-7 md:w-9"
-        />
+        <Link href="https://www.facebook.com/ToService" target="_blank">
+          <Image
+            src="facebook.svg"
+            alt="Facebook Logo"
+            width="218"
+            height="33"
+            className=" w-7 md:w-9"
+          />
+        </Link>
         <div className="flex flex-row uppercase font-extrabold gap-2 text-sm md:text-lg">
           <div className="flex flex-col gap-2">
             <p className="leading-3 text-neutral-500">FON</p>
